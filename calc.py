89e2ba081f4b61802521db0e53e35a36bed320ae
elinabhasin <elinabhasin27@gmail.com>
2024-04-29 01:01:57 +0530

    Added in few explanatory comments.

--- calc.py
+++ calc.py
@@ -17,13 +17,11 @@ buttons = [
     ["TRIG", "0", ".", "="]
 ]
 
-# Place buttons using grid
-for r, row in enumerate(buttons): #buttons is also a list of 
+for r, row in enumerate(buttons): #buttons is also a list of lists.
     for c, text in enumerate(row): #row here is a list= ["CLR", "BACK", "%", "/"]
         button = Button(root, text=text, bg="orchid3", relief='solid', borderwidth=2, width=6, height=2,command=lambda text=text:show(text))
         button.grid(row=r+1, column=c, padx=3, pady=3, sticky="nsew")
 
-# Configure rows and columns to expand proportionally
 for i in range(6):
     root.grid_rowconfigure(i, weight=1)
 for i in range(4):
@@ -86,37 +84,46 @@ def inToPost(data):
     return ans
 
 def show(text):
+
     if text=="CLR":
         display.config(text=0)
         data.clear()
+
     elif text=="BACK":
+
         if(len(data)==1 and data[0]==0):
             display.config(text=0)
+
         else:
             data.pop()
             displayStr=''.join(data)
             display.config(text=displayStr)
+
     if text>='0' and text<='9':
         data.append(text)
         displayStr=''.join(data)
         display.config(text=displayStr)
+
     if len(data)==1 and (text=='+' or text=='-' or text=='x' or text=='/' or text=='%'):
         data.append(" ")
         data.append(text)
         data.append(" ")
         displayStr=''.join(data)
         display.config(text=displayStr)
+
     if len(data)>1 and (text=='+' or text=='-' or text=='x' or text=='/' or text=='%'):
         if(data[-2]=='+' or data[-2]=='/' or data[-2]=='x' or data[-2]=='-'):
             data[-2]=text
             displayStr=''.join(data)
             display.config(text=displayStr)
+
         else:
             data.append(" ")
             data.append(text)
             data.append(" ")
             displayStr=''.join(data)
             display.config(text=displayStr)
+
     elif len(data)==0 and (text=='+' or text=='-' or text=='x' or text=='/' or text=='%'):
         data.append("0")
         data.append(" ")
@@ -124,6 +131,7 @@ def show(text):
         data.append(" ")
         displayStr=''.join(data)
         display.config(text=displayStr)
+
     if(text=='='):
         ans=inToPost(data)
         display.config(text=ans)
